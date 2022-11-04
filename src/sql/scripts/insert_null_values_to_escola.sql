b2977921cd0eda374083a8c18e5aaccb97a763ac
xpcosmos <mikeias.d.s.o@gmail.com>
2022-11-03 22:57:15 -0300

    Injestão de dados de participantes

--- src/sql/scripts/insert_null_values_to_escola.sql
+++ src/sql/scripts/insert_null_values_to_escola.sql
@@ -4,4 +4,6 @@ insert into enem2020.escolas (
     fk_cod_uf, 
     dependencia_adm, 
     tipo_loc_escola, 
-    situacao_escola) values (10322, 9,9,9,9,9) 
+    situacao_escola) values (10322, 9,9,9,9,9)
+
+
